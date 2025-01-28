44159840d172d4ec7d5d5028527a60ba2a30520c
justanyuser <basedollar15@gmail.com>
2025-01-28 20:07:25 +0100

    Your commit message

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -161,7 +161,7 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0x5b2a3f53f605d62c53e62932dac6925e3d74afa5a4b459745c36d42d0ed26a69"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
-        //vSeeds.push_back(CDNSSeedData("multidoge.org", "seed.multidoge.org", true));
+        vSeeds.push_back(CDNSSeedData("junkcoinexplorer.com", "dnsseed.junkcoinexplorer.com", true)); //anonymous
         //vSeeds.push_back(CDNSSeedData("multidoge.org", "seed2.multidoge.org"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,30);
